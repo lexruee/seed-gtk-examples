0562475f972f12a6affecc2556229ed89306c9f4
Andrew Steele <andrew@andrewsteele.me.uk>
2015-10-08 09:17:21 +0100

    Inclusion of Separator demonstrations

---
+++ separator.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env seed
+
+Gtk = imports.gi.Gtk;
+
+Gtk.init(null, null);
+
+var window = new Gtk.Window();
+window.set_title("Separator");
+window.set_default_size(400, 200);
+window.signal.hide.connect(Gtk.main_quit);
+
+var box = new Gtk.Box();
+window.add(box);
+
+var separator = new Gtk.Separator();
+separator.set_hexpand(true);
+separator.set_vexpand(true);
+box.pack_start(separator, true, true, 0);
+
+var separator = new Gtk.Separator();
+separator.set_orientation(Gtk.Orientation.VERTICAL);
+separator.set_hexpand(true);
+separator.set_vexpand(true);
+box.pack_start(separator, true, true, 0);
+
+window.show_all();
+
+Gtk.main();
