dee63406e82d9d9aa859bb90ba6a9e1e59e09233
Andrew Steele <andrew@andrewsteele.me.uk>
2015-11-12 07:03:41 +0000

    Adding example of ComboBoxText widget

---
+++ comboboxtext.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env seed
+
+const Gtk = imports.gi.Gtk;
+
+Gtk.init(null, null);
+
+function on_comboboxtext_changed()
+{
+    print("Selected vehicle is " + comboboxtext.get_active_text());
+}
+
+var window = new Gtk.Window();
+window.set_default_size(200, -1);
+window.set_title("ComboBoxText");
+window.signal.connect("destroy", Gtk.main_quit);
+
+var comboboxtext = new Gtk.ComboBoxText();
+comboboxtext.append_text("Car");
+comboboxtext.append_text("Motorcycle");
+comboboxtext.append_text("Van");
+comboboxtext.append_text("Lorry");
+comboboxtext.append_text("Truck");
+comboboxtext.set_active(0);
+comboboxtext.signal.connect("changed", on_comboboxtext_changed);
+window.add(comboboxtext);
+
+window.show_all();
+
+Gtk.main();
