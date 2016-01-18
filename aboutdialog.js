1b139f2cdc8e7c84e80efebe21bef4d1e2a16374
Andrew Steele <andrew@andrewsteele.me.uk>
2016-01-18 11:11:28 +0000

    Addition of Dialog and AboutDialog examples

---
+++ aboutdialog.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env seed
+
+Gtk = imports.gi.Gtk;
+GdkPixbuf = imports.gi.GdkPixbuf;
+
+Gtk.init(null, null);
+
+var logo = new GdkPixbuf.Pixbuf.from_file_at_size("_resources/gtk.png", 64, 64)
+
+var aboutdialog = new Gtk.AboutDialog();
+aboutdialog.set_title("AboutDialog");
+aboutdialog.set_program_name("Programmica");
+aboutdialog.set_version("1.0");
+aboutdialog.set_comments("Programming, system and network administration resources");
+aboutdialog.set_website("https://programmica.com/");
+aboutdialog.set_website_label("Programmica Website");
+aboutdialog.set_authors(["Andrew Steele"]);
+aboutdialog.set_logo(logo);
+
+aboutdialog.run();
+aboutdialog.destroy();
---
+++ dialog.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env gjs
+
+Gtk = imports.gi.Gtk;
+
+Gtk.init(null, null);
+
+var dialog = new Gtk.Dialog();
+dialog.set_title("Dialog")
+dialog.set_default_size(200, 200);
+dialog.add_button("_OK", Gtk.ResponseType.OK);
+dialog.add_button("_Cancel", Gtk.ResponseType.CANCEL);
+
+var response = dialog.run();
+
+if (response == Gtk.ResponseType.OK)
+{
+    print("OK button clicked");
+}
+else if (response == Gtk.ResponseType.CANCEL)
+{
+    print("Cancel button clicked");
+}
+
+dialog.destroy();
