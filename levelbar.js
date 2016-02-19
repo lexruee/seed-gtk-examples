a8b3912295bdb0c6ede709818f1f9b3d843fe128
Andrew Steele <andrew@andrewsteele.me.uk>
2016-02-19 12:40:45 +0000

    Addition of LevelBar widget example

---
+++ levelbar.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env seed
+
+Gtk = imports.gi.Gtk;
+
+Gtk.init(null, null);
+
+function on_entry_changed(entry)
+{
+    levelbar.set_value(entry.get_text_length());
+}
+
+var window = new Gtk.Window();
+window.set_title("LevelBar");
+window.signal.connect("destroy", Gtk.main_quit);
+
+var grid = new Gtk.Grid();
+window.add(grid);
+
+var entry = new Gtk.Entry();
+entry.signal.connect("changed", on_entry_changed);
+grid.attach(entry, 0, 0, 1, 1);
+
+var levelbar = new Gtk.LevelBar();
+levelbar.set_max_value(50);
+grid.attach(levelbar, 0, 1, 1, 1);
+
+window.show_all();
+
+Gtk.main();
