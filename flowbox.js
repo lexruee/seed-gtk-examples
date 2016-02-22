ea52969d479bcaf6cd154862da7e3b4c110e1e77
Andrew Steele <andrew@andrewsteele.me.uk>
2016-02-22 14:49:15 +0000

    Addition of FlowBox/ListBox widget files

---
+++ flowbox.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env seed
+
+Gtk = imports.gi.Gtk;
+
+Gtk.init(null, null);
+
+var window = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+window.set_title("FlowBox");
+window.set_default_size(200, -1)
+window.signal.connect("destroy", Gtk.main_quit);
+
+var flowbox = new Gtk.FlowBox();
+flowbox.set_row_spacing(5);
+flowbox.set_column_spacing(5);
+window.add(flowbox);
+
+for (var count = 1; count < 10; count++)
+{
+    var label = new Gtk.Label({label: "Label " + count});
+    flowbox.insert(label, count);
+}
+
+window.show_all();
+
+Gtk.main();
---
+++ listbox.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env seed
+
+Gtk = imports.gi.Gtk;
+
+Gtk.init(null, null);
+
+var window = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+window.set_title("ListBox");
+window.set_default_size(200, -1)
+window.signal.connect("destroy", Gtk.main_quit);
+
+var listbox = new Gtk.ListBox();
+window.add(listbox);
+
+for (var count = 1; count < 10; count++)
+{
+    var label = new Gtk.Label({label: "Label " + count});
+    listbox.insert(label, count);
+}
+
+window.show_all();
+
+Gtk.main();
