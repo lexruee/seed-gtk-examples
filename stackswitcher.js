fefa4438acf7bcdc07508b7bde92c2b47f4bc424
Andrew Steele <andrew@andrewsteele.me.uk>
2016-03-08 19:35:15 +0000

    Addition of Stack-family widget demos

---
+++ stackswitcher.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env seed
+
+Gtk = imports.gi.Gtk;
+
+Gtk.init(null, null);
+
+var window = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+window.set_title("StackSwitcher");
+window.set_default_size(200, 200);
+window.signal.connect("destroy", Gtk.main_quit);
+
+var grid = new Gtk.Grid();
+window.add(grid);
+
+var stack = new Gtk.Stack();
+stack.set_vexpand(true);
+stack.set_hexpand(true);
+grid.attach(stack, 0, 1, 1, 1);
+
+var stackswitcher = new Gtk.StackSwitcher();
+stackswitcher.set_stack(stack);
+grid.attach(stackswitcher, 0, 0, 1, 1);
+
+var i;
+
+for (i = 1; i < 6; i++)
+{
+    var name = i.toString();
+    var title = "Page " + name;
+
+    var label = new Gtk.Label({label: title});
+    stack.add_titled(label, name, title);
+}
+
+window.show_all();
+
+Gtk.main();
