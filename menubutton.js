afabe8fff0cdba518bb44b36bd6ad8821507fbff
Andrew Steele <andrew@andrewsteele.me.uk>
2016-03-23 09:22:14 +0000

    Addition of LinkButton and MenuButton

---
+++ menubutton.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env seed
+
+Gtk = imports.gi.Gtk;
+
+Gtk.init(null, null);
+
+var window = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+window.set_title("MenuButton");
+window.signal.connect("destroy", Gtk.main_quit);
+
+var menubutton = new Gtk.MenuButton({label: "MenuButton"});
+window.add(menubutton);
+
+var menu = new Gtk.Menu();
+menubutton.set_popup(menu);
+
+for (var i=1; i<6; i++)
+{
+    var menuitem = new Gtk.MenuItem({label: "MenuItem " + i});
+    menu.append(menuitem);
+}
+
+menu.show_all();
+window.show_all();
+
+Gtk.main();
